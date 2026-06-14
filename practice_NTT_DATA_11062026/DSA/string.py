6aa46b11f518cde1e93fc5837ed783fe4fff530c
kanhasahu955 <kanhasahu8018183830@gmail.com>
2026-06-15 01:19:51 +0530

    python interview questions

---
+++ practice_NTT_DATA_11062026/DSA/string.py
@@ -0,0 +1,9 @@
+# count characters in a string
+s = "hello world"
+char_count = {}
+for char in s:
+    if char in char_count:
+        char_count[char] += 1
+    else:
+        char_count[char] = 1    
+print(char_count)
